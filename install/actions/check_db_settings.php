41683738f6833ae0e6410d9290ae182050585743
Aleksandr Mikhalkin <121282426+Afistotel@users.noreply.github.com>
2026-01-29 15:12:24 +0300

    Fix

--- install/actions/check_db_settings.php
+++ install/actions/check_db_settings.php
@@ -1,37 +1,52 @@
 <?php
-  $server = trim($_POST['db_host']);
-  $port = trim($_POST['db_port']);
-  $username = trim($_POST['db_username']);
-  $password = trim($_POST['db_password']);
-  $database = trim($_POST['db_name']);
-  
-  $params = array('db_host'=>$_POST['db_host'],'db_port'=>$_POST['db_port'],'db_username'=>$_POST['db_username'],'db_password'=>$_POST['db_password'],'db_name'=>$_POST['db_name'],'lng'=>$_GET['lng']);
-    
-  db_connect($server,$username, $password,$database,$port,'db_link',$params);
-      
-//check user privileges    
-  $user_privileges_list = array();
-  $user_privileges_query = db_query("SHOW PRIVILEGES");
-  while($user_privileges = db_fetch_array($user_privileges_query))
-  {  
-    $user_privileges_list[] = $user_privileges['Privilege'];    
-  }
-              
-  $required_privileges = array('Select','Insert','Update','Delete','Create','Drop','Alter');
-  
-  $missed_privileges = array();
-  foreach($required_privileges as $v)
-  {
-    if(!in_array($v,$user_privileges_list))
-    {
-      $missed_privileges[] = $v;
-    }
-  }  
-  
-  if(count($missed_privileges)>0)
-  {
-    $error = 'Next privileges: "' . implode(',',$missed_privileges) . '" are required for mysql user.';
-    header('Location: index.php?step=database_config&db_error=' . urlencode($error) . '&lng=' .(isset($params['lng']) ? $params['lng']:''). '&params=' . base64_encode(json_encode($params)));
-    exit();
-  }
+// Получение и очистка входных данных
+$server   = trim($_POST['db_host'] ?? '');
+$port     = trim($_POST['db_port'] ?? '');
+$username = trim($_POST['db_username'] ?? '');
+$password = trim($_POST['db_password'] ?? '');
+$database = trim($_POST['db_name'] ?? '');
 
+// Формирование параметров для передачи
+$params = [
+    'db_host'     => $server,
+    'db_port'     => $port,
+    'db_username' => $username,
+    'db_password' => $password,
+    'db_name'     => $database,
+    'lng'         => $_GET['lng'] ?? ''
+];
+
+// Подключение к БД
+db_connect($server, $username, $password, $database, $port, 'db_link', $params);
+
+// Проверка привилегий пользователя
+$userPrivilegesList = [];
+$result = db_query('SHOW PRIVILEGES');
+
+while ($row = db_fetch_array($result)) {
+    $userPrivilegesList[] = $row['Privilege'];
+}
+
+// Требуемые привилегии
+$requiredPrivileges = ['Select', 'Insert', 'Update', 'Delete', 'Create', 'Drop', 'Alter'];
+
+// Поиск отсутствующих привилегий
+$missedPrivileges = array_filter($requiredPrivileges, function ($privilege) use ($userPrivilegesList) {
+    return !in_array($privilege, $userPrivilegesList, true);
+});
+
+// Обработка ошибок
+if (!empty($missedPrivileges)) {
+    $error = 'Next privileges: "' . implode(', ', $missedPrivileges) . '" are required for MySQL user.';
+    
+    $queryParams = [
+        'step'      => 'database_config',
+        'db_error'  => $error,
+        'lng'       => $params['lng'],
+        'params'    => base64_encode(json_encode($params))
+    ];
+    
+    $queryString = http_build_query($queryParams);
+    header('Location: index.php?' . $queryString);
+    exit();
+}
--- install/index.php
+++ install/index.php
@@ -1,199 +1,150 @@
 <?php
-    error_reporting(E_ALL & ~E_NOTICE);
-        
+// Отключаем уведомления, оставляем остальные ошибки
+error_reporting(E_ALL & ~E_NOTICE);
 
-    define('PROJECT_VERSION', '3.6.3');
+// Версия проекта
+define('PROJECT_VERSION', '3.6.3');
 
-    // set default timezone if none exists (PHP 5.3 throws an E_WARNING)
-    define('CFG_TIME_ZONE', 'Europe/Moscow');
+require_once 'lib/database.php';
+require_once 'lib/html.php';
 
-    if (PHP_VERSION >= '5.2')
-    {
-        date_default_timezone_set(defined('CFG_TIME_ZONE') ? CFG_TIME_ZONE : date_default_timezone_get());
+// Редирект, если задан step, но нет lng
+if (isset($_GET['step']) && !isset($_GET['lng'])) {
+    header('Location: index.php');
+    exit();
+}
+
+// Массив доступных языков и соответствующих файлов
+$availableLanguages = [
+    'russian'  => 'languages/russian.php',
+    'chinese'  => 'languages/chinese.php',
+    'italian'  => 'languages/italian.php',
+    'english'  => 'languages/english.php',
+    'german'   => 'languages/german.php'
+];
+
+// Подключаем языковой файл
+$language = $_GET['lng'] ?? 'english';
+$languageFile = $availableLanguages[$language] ?? $availableLanguages['english'];
+require_once $languageFile;
+
+// Формируем заголовок приложения
+$app_title = isset($_GET['lng'])
+    ? sprintf(TEXT_INSTALLATION_HEADING, PROJECT_VERSION)
+    : sprintf('Rukovoditel %s Installation', PROJECT_VERSION);
+
+// Получаем параметры запроса
+$step   = $_GET['step'] ?? '';
+$action = $_GET['action'] ?? '';
+
+// Проверка установки приложения
+$configPath = substr(__DIR__, 0, -7) . 'config/database.php';
+if (is_file($configPath) && $step !== 'success') {
+    header('Location: ../index.php');
+    exit();
+}
+
+// Подключение действий по шагам
+$actions = [
+    'rukovoditel_config' => 'actions/check_db_settings.php',
+    'install_rukovoditel' => 'actions/install_rukovoditel.php'
+];
+
+foreach ($actions as $key => $file) {
+    if ($step === $key || $action === $key) {
+        require_once $file;
+        break;
     }
-
-    include('lib/database.php');
-
-    include('lib/html.php');
-
-    if (isset($_GET['step']) and !isset($_GET['lng']))
-    {
-        header('Location: index.php');
-        exit();
-    }
-
-    if (isset($_GET['lng']))
-    {
-        switch ($_GET['lng'])
-        {
-            case 'russian':
-                include('languages/russian.php');
-                break;
-            case 'chinese':
-                include('languages/chinese.php');
-                break;
-            case 'italian':
-                include('languages/italian.php');
-                break;	
-            case 'english':
-                include('languages/english.php');
-                break;
-            case 'german':
-                include('languages/german.php');
-                break;
-        }
-
-        $app_title = sprintf(TEXT_INSTALLATION_HEADING, PROJECT_VERSION);
-    }
-    else
-    {
-        include('languages/english.php');
-        
-        $app_title = sprintf('Rukovoditel %s Installation', PROJECT_VERSION);
-    }
-
-    $step = $_GET['step'] ?? '';
-    $action = $_GET['action'] ?? '';
-    
-    
-    //Check if app is intalled    
-    if(is_file(substr(__DIR__,0,-7) . 'config/database.php') and $step!='success')
-    {
-       header('Location: ../index.php'); 
-    }
-
-
-    if ($step == 'rukovoditel_config')
-        include('actions/check_db_settings.php');
-
-    if ($action == 'install_rukovoditel')
-        include('actions/install_rukovoditel.php');
+}
 ?>
-
 
 <!DOCTYPE html>
-<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
-<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
-<!--[if !IE]><!-->
 <html lang="en" class="no-js">
-<!--<![endif]-->
-<!-- BEGIN HEAD -->
 <head>
-<meta charset="utf-8"/>
-<title><?php echo $app_title ?></title>
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-<meta content="" name="description"/>
-<meta content="" name="author"/>
-<meta name="MobileOptimized" content="320">
+    <meta charset="utf-8"/>
+    <title><?= $app_title ?></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <meta name="MobileOptimized" content="320">
 
-<!-- BEGIN GLOBAL MANDATORY STYLES -->
-<link href="../template/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-<link href="../template/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="../template/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
-<!-- END GLOBAL MANDATORY STYLES -->
-<!-- BEGIN PAGE LEVEL STYLES -->
-<link rel="stylesheet" type="text/css" href="../template/plugins/select2/select2_conquer.css"/>
-<!-- END PAGE LEVEL SCRIPTS -->
-<!-- BEGIN THEME STYLES -->
-<link href="../template/css/style-conquer.css" rel="stylesheet" type="text/css"/>
-<link href="../template/css/style.css" rel="stylesheet" type="text/css"/>
-<link href="../template/css/style-responsive.css" rel="stylesheet" type="text/css"/>
-<link href="../template/css/plugins.css" rel="stylesheet" type="text/css"/>
-<link href="../css/skins/default/default.css" rel="stylesheet" type="text/css" />
+    <!-- Стили -->
+    <link href="../template/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <link href="../template/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="../template/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" type="text/css" href="../template/plugins/select2/select2_conquer.css"/>
+    <link href="../template/css/style-conquer.css" rel="stylesheet" type="text/css"/>
+    <link href="../template/css/style.css" rel="stylesheet" type="text/css"/>
+    <link href="../template/css/style-responsive.css" rel="stylesheet" type="text/css"/>
+    <link href="../template/css/plugins.css" rel="stylesheet" type="text/css"/>
+    <link href="../css/skins/default/default.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" type="text/css" href="../css/default.css"/>
 
-<style>
-.login .content{
-  width: auto;
-  max-width: 750px;
-}
-</style>
+    <style>
+        .login .content {
+            width: auto;
+            max-width: 750px;
+        }
+    </style>
 
-<script src="js/jquery/3.6.4/jquery-3.6.4.min.js" type="text/javascript"></script>
+    <!-- Скрипты -->
+    <script src="js/jquery/3.6.4/jquery-3.6.4.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/validation/1.9.5/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="js/validation/1.9.5/additional-methods.min.js"></script>
+    <script type="text/javascript" src="../js/main.js"></script>
 
-<script type="text/javascript" src="js/validation/1.9.5/jquery.validate.min.js"></script>
-<script type="text/javascript" src="js/validation/1.9.5/additional-methods.min.js"></script>
- 
-<script type="text/javascript" src="../js/main.js"></script>
+    <script type="text/javascript">
+        $.extend($.validator.messages, {
+            required: '<?= TEXT_FIELD_IS_REQURED ?>',
+            email: '<?= TEXT_FIELD_IS_REQURED_EMAIL ?>'
+        });
+    </script>
 
-<link rel="stylesheet" type="text/css" href="../css/default.css"/>
-
-<script type="text/javascript">
-  $.extend($.validator.messages, { 
-    required: '<?php echo TEXT_FIELD_IS_REQURED ?>',
-    email: '<?php echo TEXT_FIELD_IS_REQURED_EMAIL ?>'    
-  });
-</script> 
-
-
-<!-- END THEME STYLES -->
-<link rel="shortcut icon" href="../favicon.ico"/>
+    <link rel="shortcut icon" href="../favicon.ico"/>
 </head>
-<!-- BEGIN BODY -->
 <body class="login">
-<!-- BEGIN LOGO -->
-<div class="login-page-logo"><?php echo $app_title ?></div>
-<!-- END LOGO -->
-<!-- BEGIN LOGIN -->
-<div class="content">
+    <!-- LOGO -->
+    <div class="login-page-logo"><?= $app_title ?></div>
 
-<?php
-    switch($step)
-    {
-        case 'checking_environment':
-            include('modules/checking_environment.php');
-            break;
-        case 'database_config':
-            include('modules/database_config.php');
-            break;
-        case 'rukovoditel_config':
-            include('modules/rukovoditel_config.php');
-            break;
-        case 'success':
-            include('modules/success.php');
-            break;
-        default:
-            include('modules/language.php');
-            break;
-        
-    }
-?>
-    
-</div>
-<!-- END LOGIN -->
-<!-- BEGIN COPYRIGHT -->
-<div class="copyright">
-	 <a href="https://www.rukovoditel.net" target="_blank">Rukovoditel <?php echo PROJECT_VERSION ?></a><br>
-    Copyright &copy; <?php echo date('Y')?> <a target="_blank" href="https://www.rukovoditel.net">www.rukovoditel.net</a>
-</div>
-<!-- END COPYRIGHT -->
-<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
-<!-- BEGIN CORE PLUGINS -->
-<!--[if lt IE 9]>
-<script src="../template/plugins/respond.min.js"></script>
-<script src="../template/plugins/excanvas.min.js"></script> 
-<![endif]-->
-<script src="../template/plugins/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
-<script src="../template/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../template/plugins/bootstrap-hover-dropdown/twitter-bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
-<script src="../template/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-<script src="../template/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-<script src="../template/plugins/jquery.cokie.min.js" type="text/javascript"></script>
-<script src="../template/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
-<!-- END CORE PLUGINS -->
-<!-- BEGIN PAGE LEVEL PLUGINS -->
-<script type="text/javascript" src="../template/plugins/select2/select2.min.js"></script>
-<!-- END PAGE LEVEL PLUGINS -->
-<!-- BEGIN PAGE LEVEL SCRIPTS -->
-<script src="../template/scripts/app.js" type="text/javascript"></script>
-<!-- END PAGE LEVEL SCRIPTS -->
+    <!-- LOGIN -->
+    <div class="content">
+        <?php
+        $modules = [
+            'checking_environment' => 'modules/checking_environment.php',
+            'database_config'      => 'modules/database_config.php',
+            'rukovoditel_confi'    => 'modules/rukovoditel_config.php',
+            'success'              => 'modules/success.php'
+        ];
 
-<script>
-jQuery(document).ready(function() {     
-  App.init();
-});
-</script>
-<!-- END JAVASCRIPTS -->
+        $moduleFile = $modules[$step] ?? 'modules/language.php';
+        require_once $moduleFile;
+        ?>
+    </div>
+
+    <!-- COPYRIGHT -->
+    <div class="copyright">
+        <a href="https://www.rukovoditel.net" target="_blank">Rukovoditel <?= PROJECT_VERSION ?></a><br>
+        Copyright &copy; <?= date('Y') ?> <a target="_blank" href="https://www.rukovoditel.net">www.rukovoditel.net</a>
+    </div>
+
+    <!-- JAVASCRIPTS -->
+    <!--[if lt IE 9]>
+    <script src="../template/plugins/respond.min.js"></script>
+    <script src="../template/plugins/excanvas.min.js"></script> 
+    <![endif]-->
+    <script src="../template/plugins/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
+    <script src="../template/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="../template/plugins/bootstrap-hover-dropdown/twitter-bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+    <script src="../template/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+    <script src="../template/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+    <script src="../template/plugins/jquery.cokie.min.js" type="text/javascript"></script>
+    <script src="../template/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="../template/plugins/select2/select2.min.js"></script>
+    <script src="../template/scripts/app.js" type="text/javascript"></script>
+
+    <script>
+        jQuery(document).ready(function() {
+            App.init();
+        });
+    </script>
 </body>
-<!-- END BODY -->
 </html>
